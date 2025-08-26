263b42a0a8cca449d50d2ab0eaa0ee926e7b49d6
Jessie Finocchiaro <22jjf22@gmail.com>
2025-08-26 16:52:19 -0400

    change ps1_part1 and add test

--- ps1_part1.py
+++ ps1_part1.py
@@ -1,18 +1,10 @@
-# Ask the user for the first number
-firstnum = int(input("Enter an integer between 10 and 100: "))
-if not (10 <= firstnum <= 100):
-    raise ValueError("The first number must be between 10 and 100.")
+# Ask the user for the first number, store the value in a variable
 
-# Ask the user for the second number
-secondnum = int(input("Enter an integer less than 4: "))
-if not (secondnum < 4):
-    raise ValueError("The second number must be less than 4.")
+
+# Ask the user for the second number, store the value in a variable
+
 
 # Repeat back the numbers
-print(f"You entered {firstnum} and {secondnum}")
 
-# Perform calculations
-print(f"{firstnum} + {secondnum} = {firstnum + secondnum}")
-print(f"{firstnum} * {secondnum} = {firstnum * secondnum}")
-print(f"{firstnum} ** {secondnum} = {firstnum ** secondnum}")
-print(f"{firstnum} % {secondnum} = {firstnum % secondnum}")
+
+# Perform calculations. Be careful about string formatting for autograders.
